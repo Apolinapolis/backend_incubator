e79186a6ff51802664cc673499a833b5aca8ab43
Apolinapolis <larra12@ya.ru>
2025-06-17 18:31:24 +0300

    fix - one test file

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,29 @@
-const { createDefaultPreset } = require("ts-jest");
+// const { createDefaultPreset } = require("ts-jest");
+
+// const tsJestTransformCfg = createDefaultPreset().transform;
+
+// /** @type {import("jest").Config} **/
+// module.exports = {
+//   testEnvironment: "node",
+//   transform: {
+//     ...tsJestTransformCfg,
+//   },
+// };
+
+import type { Config } from 'jest';
+import { createDefaultPreset } from 'ts-jest';
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
-/** @type {import("jest").Config} **/
-module.exports = {
-  testEnvironment: "node",
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   transform: {
     ...tsJestTransformCfg,
   },
+  testMatch: ['<rootDir>/__tests__/**/*.tests.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
+
+export default config;
